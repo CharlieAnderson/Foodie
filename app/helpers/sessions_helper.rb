7541994bb517e40b35390a170ba0e8bd930590bf
Charlie Anderson <charlieanderson13@gmail.com>
2017-04-21 12:23:03 -0700

    completed section 8 basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+    
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -13,4 +18,6 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+
 end
